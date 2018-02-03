3816ccd15f13f0b6e71a096aa2393233bcba7269
lapie <lapie@192.168.2.220>
2018-02-03 16:42:29 -0500

    Fraction test

--- src/javatd2/JavaTD2.java
+++ src/javatd2/JavaTD2.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package javatd2;
+import java.util.*;
 
 /**
  *
@@ -16,6 +17,81 @@ public class JavaTD2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entreeClavier = new Scanner(System.in);
+        
+//        Fraction f1 = new Fraction(4,6);
+//        Fraction f2 = new Fraction("6/3");
+//        
+//        System.out.println(f1.toString());
+//        System.out.println(f2.toString());
+        
+        //tester addition 
+        Fraction f1;
+        Fraction f3;
+        
+        System.out.println("Donnez deux entiers : ");
+        
+        int c = entreeClavier.nextInt();
+        int d = entreeClavier.nextInt();
+        
+        f1 = new Fraction(c,d);
+        f1 = f1.formeReduite();
+        
+        System.out.println(f1.toString());
+        
+        Fraction f;
+        System.out.println("Donnez deux entiers : ");
+        
+        int a = entreeClavier.nextInt();
+        int b = entreeClavier.nextInt();
+        
+        f = new Fraction(a,b);
+        f = f.formeReduite();
+        System.out.println(f.toString());
+        
+        // additionne f et f1 dans un resultat f3
+        f3 = f1.addition(f);
+        System.out.println("l'addition de " + f1 + " + " + f + " = " +  f3.toString());
+        
+        //soustraction 
+        f3 = f1.soustraction(f);
+        System.out.println("la soustraction de " + f1 + " - " + f + " = " +  f3.toString());
+        
+        //multiplication  
+        f3 = f1.multiplication(f);
+        System.out.println("la multiplication de " + f1 + " * " + f + " = " +  f3.toString());
+        
+        //division  
+        f3 = f1.division(f);
+        f3.formeReduite();
+         
+        System.out.println("la multiplication de " + f1 + " / " + f + " = " +  f3.toString());
+        
+        
+        // tester la puissance 
+        System.out.println("Entrez une puissance : ");
+        int pow = entreeClavier.nextInt();
+        
+        f3 = f1.puissance(pow); 
+        f3.formeReduite();
+        
+        System.out.println("la fraction " + f1 + " à la puissance " + pow + " = " +  f3.toString());
+        
+        
+        
+        
+        //tester forme reduite 
+//        Fraction f;
+//        System.out.println("Donnez deux entiers : ");
+//        
+//        int a = entreeClavier.nextInt();
+//        int b = entreeClavier.nextInt();
+//        
+//        f = new Fraction(a,b);
+//        f = f.formeReduite();
+//        System.out.println(f.connaitreFormeReduite());
+        
+        
         
         
     }
